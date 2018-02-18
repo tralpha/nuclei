ffdd3f38650e4259d688fbf869ae8a92995c1a51
Ralph Tigoumo <ralphwantek@gmail.com>
2018-02-18 08:39:29 +0000

    Add iou metric functions for the DSB

--- solutions/kmrcnn/utils.py
+++ solutions/kmrcnn/utils.py
@@ -18,6 +18,7 @@ import skimage.color
 import skimage.io
 import urllib.request
 import shutil
+from skimage.morphology import label
 
 # URL from which to download the latest COCO trained weights
 COCO_MODEL_URL = "https://github.com/matterport/Mask_RCNN/releases/download/v2.0/mask_rcnn_coco.h5"
@@ -706,3 +707,91 @@ def download_trained_weights(coco_model_path, verbose=1):
         shutil.copyfileobj(resp, out)
     if verbose > 0:
         print("... done downloading pretrained model!")
+
+
+############################################################
+#  Data Science Bowl Functions
+############################################################
+
+
+
+def iou_metric(y_true_in, y_pred_in, print_table=False):
+    # Some important assertions
+    assert y_true_in.shape[:2] == y_pred_in.shape[:2], "Check metric"
+    # Is the below required? Are you sure?
+    # labels = label(y_true_in > 0.5)
+    # y_pred = label(y_pred_in > 0.5)
+    height, width = y_true_in.shape[:2]
+    labels = np.zeros((height, width), np.uint16)
+    y_pred = np.zeros((height, width), np.uint16)
+
+    n_gt_masks = y_true_in.shape[-1]
+    n_pred_masks = y_pred_in.shape[-1]
+
+    for index in range(0,n_gt_masks):
+        labels[y_true_in[:,:,index] > 0] = index + 1
+
+    for index in range(0,n_pred_masks):
+        y_pred[y_pred_in[:,:,index] > 0] = index + 1
+
+    
+    true_objects = len(np.unique(labels))
+    pred_objects = len(np.unique(y_pred))
+
+    intersection = np.histogram2d(labels.flatten(), y_pred.flatten(), bins=(true_objects, pred_objects))[0]
+
+    # Compute areas (needed for finding the union between all objects)
+    area_true = np.histogram(labels, bins = true_objects)[0]
+    area_pred = np.histogram(y_pred, bins = pred_objects)[0]
+    area_true = np.expand_dims(area_true, -1)
+    area_pred = np.expand_dims(area_pred, 0)
+
+    # Compute union
+    union = area_true + area_pred - intersection
+
+    # Exclude background from the analysis
+    intersection = intersection[1:,1:]
+    union = union[1:,1:]
+    union[union == 0] = 1e-9
+
+    # Compute the intersection over union
+    iou = intersection / union
+
+    # Precision helper function
+    def precision_at(threshold, iou):
+        matches = iou > threshold
+        true_positives = np.sum(matches, axis=1) == 1   # Correct objects
+        false_positives = np.sum(matches, axis=0) == 0  # Missed objects
+        false_negatives = np.sum(matches, axis=1) == 0  # Extra objects
+        tp, fp, fn = np.sum(true_positives), np.sum(false_positives), np.sum(false_negatives)
+        return tp, fp, fn
+
+    # Loop over IoU thresholds
+    prec = []
+    if print_table:
+        print("Thresh\tTP\tFP\tFN\tPrec.")
+    for t in np.arange(0.5, 1.0, 0.05):
+        tp, fp, fn = precision_at(t, iou)
+        if (tp + fp + fn) > 0:
+            p = tp / (tp + fp + fn)
+        else:
+            p = 0
+        if print_table:
+            print("{:1.3f}\t{}\t{}\t{}\t{:1.3f}".format(t, tp, fp, fn, p))
+        prec.append(p)
+    
+    if print_table:
+        print("AP\t-\t-\t-\t{:1.3f}".format(np.mean(prec)))
+    return np.mean(prec)
+
+def iou_metric_batch(y_true_in, y_pred_in):
+    batch_size = y_true_in.shape[0]
+    metric = []
+    for batch in range(batch_size):
+        value = iou_metric(y_true_in[batch], y_pred_in[batch])
+        metric.append(value)
+    return np.array(np.mean(metric), dtype=np.float32)
+
+def my_iou_metric(label, pred):
+    metric_value = tf.py_func(iou_metric_batch, [label, pred], tf.float32)
+    return metric_value
